a447699909d11b7455115b64bbca4e5d976a2993
wandeltk <wandeltk@gmail.com>
2020-04-25 13:57:18 -0600

    updated ingredient data (added mock data)'

---
+++ src/data/ingredient-data.ts
@@ -0,0 +1,34 @@
+import { Ingredient } from '../models/ingredient';
+
+export default [
+
+    new Ingredient('flour tortilla', 'unit', 140, 23, 3, 4),
+    new Ingredient('chicken', 'oz', 30, 0, 6, 1),
+    new Ingredient('avocado', 'unit', 160, 8, 2, 14),
+    new Ingredient('lime', 'unit', 11, 4, 0, 0),
+    new Ingredient('cilantro', 'cup', 23, 4, 2, 1),
+    new Ingredient('white onion', 'unit', 64, 15, 2, 1),
+    new Ingredient('tilapia', 'unit', 100, 0, 20, 3),
+    new Ingredient('couscous', 'cup', 176, 36, 6, 0), 
+    new Ingredient('asparagus', 'unit', 20, 4, 2, 0),
+    new Ingredient('butter', 'tbsp', 70, 0, 0, 11),
+    new Ingredient('lemon juice', 'tbsp', 10, 0, 0, 0), 
+    new Ingredient('thyme', 'N/A', 0, 0, 0, 0),
+    new Ingredient('basil', 'N/A', 0, 0, 0, 0),
+    new Ingredient('rice', 'cup', 206, 70, 4, 0),
+    new Ingredient('yogurt', 'cup', 220, 9, 22, 11),
+    new Ingredient('curry spices', 'N/A', 0, 0, 0, 0),
+    new Ingredient('white bread', 'unit', 60, 13, 2, 1),
+    new Ingredient('peanut butter', 'tbsp', 105, 4, 4, 8),
+    new Ingredient('grape jelly', 'tbsp', 50, 13, 0, 0),
+    new Ingredient('beef', 'oz', 40, 1, 8, 2),
+    new Ingredient('corn tortilla', 'unit', 50, 10, 1, 1),
+    new Ingredient('potato', 'unit', 110, 26, 3, 0),
+    new Ingredient('gravy packet', 'unit', 25, 4, 0, 1),
+    new Ingredient('carrot', 'unit', 41, 10, 1, 0),
+    new Ingredient('shrimp', 'oz', 20, 1, 4, 1), 
+    new Ingredient('angel-hair pasta', 'cup', 210, 41, 7, 1),
+    new Ingredient('garlic', 'unit', 3, 1, 0, 0),
+    new Ingredient('cream', 'tbsp', 45, 0, 0, 5)
+
+]
