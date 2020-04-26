87810c9190e21cdab537f36e2251668aa9caff03
wandeltk <wandeltk@gmail.com>
2020-04-26 15:18:46 -0600

    corrected total nutrient calculations

--- src/models/recipe.ts
+++ src/models/recipe.ts
@@ -1,12 +1,34 @@
 import { Ingredient } from './ingredient'
 
+
 export class Recipe {
 
     name: string;
-    ingredients: Ingredient; 
+    totalCal: number;
+    totalCarbs: number;
+    totalProtien: number;
+    totalFats: number;
+    ingredients: Ingredient[]; 
 
-    constructor(name: string, {...ingredients}: Ingredient) {
+    constructor(name: string, ingredients: Ingredient[]) {
         this.name = name;
         this.ingredients = ingredients;
+
+        let cals = 0;
+        let carbs = 0;
+        let protiens = 0;
+        let fats = 0;
+
+        for (let i = 0; i < ingredients.length; i++) {
+            cals  = cals + ingredients[i].calories;
+            carbs  = carbs + ingredients[i].carbs;
+            protiens  = protiens + ingredients[i].protien;
+            fats = fats + ingredients[i].fats;
+        }
+        
+        this.totalCal = cals;
+        this.totalCarbs = carbs
+        this.totalProtien = protiens;
+        this.totalFats = fats;
     }
 }
