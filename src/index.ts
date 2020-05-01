cbdc5d86996a4e5f91cccf90825ec14472531c49
wandeltk <wandeltk@gmail.com>
2020-04-30 20:34:16 -0600

    added express and @types/express as dependency

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express from 'express';
-import fs from 'fs';
-import morgan from 'morgan';
-import path from 'path';
 import { IngredientRouter } from './routers/ingredient-router';
 import { RecipeRouter } from '/routers/recipe-router';
 import { MealplanRouter} from '/routers/mealplan-router';
 
 const app = express();
+
+
 
 app.listen(8080, () => {
     console.log('Application running and listening at: http://localhost:8080');
