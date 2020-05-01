acfd8cda454d4ba5b9230f04956e9cfac2f5646e
wandeltk <wandeltk@gmail.com>
2020-05-01 08:52:45 -0600

    added pg config

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
+import dotenv from 'dotenv';
+import { Pool } from 'pg';
 import express from 'express';
 import { IngredientRouter } from './routers/ingredient-router';
 //import { RecipeRouter } from '/routers/recipe-router';
 //import { MealplanRouter} from '/routers/mealplan-router';
 
+//environment config
+dotenv.config();
+
+//database config
+export const connectionPool: Pool = new Pool({
+    host: process.env['DB_HOST'],
+    port: +process.env['DB_PORT'],
+    database: process.env['DB_NAME'],
+    user: process.env['DB_USERNAME'],
+    password: process.env['DB_PASSWORD'],
+    max: 5
+});
+
+//webserver config
 const app = express();
 
 app.use('/', express.json());
