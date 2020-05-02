c650e15b27e557b66392b675060e518b81836746
wandeltk <wandeltk@gmail.com>
2020-05-01 19:43:16 -0600

    editted recipe model to comply with EDR

--- src/models/recipe.ts
+++ src/models/recipe.ts
@@ -4,31 +4,10 @@ import { Ingredient } from './ingredient';
 export class Recipe {
 
     name: string;
-    totalCal: number;
-    totalCarbs: number;
-    totalProtien: number;
-    totalFats: number;
-    ingredients: Ingredient[]; 
+    servings: number;
 
-    constructor(name: string, ingredients: Ingredient[]) {
+    constructor(name: string, servings? : number) {
         this.name = name;
-        this.ingredients = ingredients;
-
-        let cals = 0;
-        let carbs = 0;
-        let protiens = 0;
-        let fats = 0;
-
-        for (let i = 0; i < ingredients.length; i++) {
-            cals  = cals + ingredients[i].calories;
-            carbs  = carbs + ingredients[i].carbs;
-            protiens  = protiens + ingredients[i].protien;
-            fats = fats + ingredients[i].fats;
-        }
-        
-        this.totalCal = cals;
-        this.totalCarbs = carbs;
-        this.totalProtien = protiens;
-        this.totalFats = fats;
+        this.servings = servings;
     }
 }
